43e3811726d2f78fbaa7a1bfed116ed9149ac6b9
astrax <ammarahmed.ph@gmail.com>
2016-09-16 20:43:29 +0100

    Daynight VLF map

--- MyMaps/DayNighTransmitters.py
+++ MyMaps/DayNighTransmitters.py
@@ -17,10 +17,10 @@ with open(filename) as f:
     # Store the latitudes and longitudes in the appropriate lists.
     for row in reader:
         labels.append(row[0])
-        lats.append(float(row[1]))
-        lons.append(float(row[2]))
-        tunlon.append(float(row[4]))
-        tunlat.append(float(row[5]))
+        lats.append(float(row[2]))
+        lons.append(float(row[3]))
+        tunlon.append(float(row[5]))
+        tunlat.append(float(row[6]))
 #Tunis coordinate
 tunis_lat=36.5
 tunis_lon=10.08
@@ -30,35 +30,36 @@ import matplotlib.pyplot as plt
 #from matplotlib.widgets import Cursor
 import numpy as np
 from datetime import datetime
-map = plt.figure(figsize=[7,5]) 
+map = plt.figure(figsize=[10,7]) 
 map = Basemap(projection='mill', area_thresh = 1000.0,
               lat_0=0, lon_0=10, resolution = 'l', suppress_ticks=True)      #resolution : 'l' for low and 'h' for hight and 'c'
 
 
-map.drawcoastlines()
-map.drawcountries()
+map.drawcoastlines(color='#A3A3A3')
+map.drawcountries(color='#A3A3A3')
 map.drawmapboundary(fill_color='lightblue')
-map.fillcontinents(color='#FFD39B',lake_color='lightblue')
-map.drawparallels(np.arange(-90,90,30),labels=[1,0,0,0])
-map.drawmeridians(np.arange(map.lonmin,map.lonmax+30,60),labels=[0,0,0,1])
+map.fillcontinents(color='#FFFFFF',lake_color='lightblue')
+map.drawparallels(np.arange(-90,100,30),labels=[1,0,0,0],color='#A1A866', linewidth=.2)
+map.drawmeridians(np.arange(map.lonmin,map.lonmax+30,30),labels=[0,0,0,1],color='#A1A866', linewidth=.2)
  
 x,y = map(lons, lats)
 v,w = map(tunis_lon, tunis_lat)
-map.plot(x, y, 'ro', markersize=7, label='VLF transmitters')
-map.plot(v, w, 'y^', markersize=7, label='LSAMA-Tunisia')
+map.plot(x, y, 'ro', markersize=10, label='VLF Transmitter')
+map.plot(v, w, 'bo', markersize=10, label='VLF Reciever')
 for label, xpt, ypt in zip(labels, x, y):
-    plt.text(xpt+10000, ypt+5000, label, color='purple',fontsize=10, fontweight='bold')
+    plt.text(xpt+10000, ypt+5000, label, color='k',fontsize=14, fontweight='bold')
 
 for wpt, vpt, xpt, ypt in zip(tunlon, tunlat, lons, lats):
     # draw great circle route between tunisia and vlf trx
    
-    map.drawgreatcircle(wpt,vpt,xpt,ypt,linewidth=1, color='r')
+    map.drawgreatcircle(wpt,vpt,xpt,ypt,linewidth=2, linestyle='--',color='k')
     
 # shade the night areas, with alpha transparency so the
 # map shows through. Use current time in UTC.
-date = datetime.today()
+#date = datetime.today()
+date=datetime(2016,6, 21,12)
 CS=map.nightshade(date)
-plt.title('Day/Night Map for %s (UTC)' % date.strftime("%d %b %Y %H:%M:%S"))
+#plt.title('Day/Night Map for %s (UT)' % date.strftime("%d %b %Y %H:%M:%S"))
 
 #to show long/lat coordination under the curosor
 ax = plt.gca()
@@ -73,5 +74,5 @@ ax.format_coord = format_coord
 #make legend
 legend = plt.legend(loc='lower left',fontsize=8,frameon=True,title='Legend',markerscale=1,prop={'size':10})
 legend.get_title().set_fontsize('20')
-
+plt.savefig("carte_vlf_dn.pdf", dps=50)
 plt.show()
